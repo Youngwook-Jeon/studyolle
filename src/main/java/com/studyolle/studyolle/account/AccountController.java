8844c248255227078c41e62a1dbe2c0f061b5cfe
Young wook Jeon <ela87@naver.com>
2021-03-01 20:13:14 +0900

    Add sign-up page

--- src/main/java/com/studyolle/studyolle/account/AccountController.java
+++ src/main/java/com/studyolle/studyolle/account/AccountController.java
@@ -1,6 +1,5 @@
 package com.studyolle.studyolle.account;
 
-import com.studyolle.studyolle.domain.Account;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,10 +9,7 @@ public class AccountController {
 
     @GetMapping("/sign-up")
     public String signUp(Model model) {
-        Account account = new Account();
-        model.addAttribute("nickname", account.getNickname());
-        model.addAttribute("email", account.getEmail());
-        model.addAttribute("password", account.getPassword());
+        model.addAttribute(new SignUpForm());
 
         return "account/sign-up";
     }
